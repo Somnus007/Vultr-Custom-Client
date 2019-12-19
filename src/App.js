ce7831e9b2474c4fa27f9648a3c0961c30475180
Allen <allen.song@h8tch.com>
2019-12-19 17:26:38 +0800

    test

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  console.log(111);
+  console.log(111); //eslint-disable-line
   return (
     <div className="App">
-      <header className="App-header haha hehe heihei dede nana meme cscs sdsdsdsssssssssssssd sdsesdsdsdsdsdsdsdsdsdsdsdsd def">
+      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
