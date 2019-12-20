e45905cb607a581a07f2bf9b50f1803b4de3c384
Allen <allen.song@h8tch.com>
2019-12-20 13:48:27 +0800

    Update construct of project

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
+import './assets/styles/global.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
