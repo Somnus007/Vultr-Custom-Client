8f9debf8761efcf52a2fb5badf99ebc4492af230
Somnus007 <somnus.sxy@gmail.com>
2020-06-11 09:13:44 +0000

    Deploying to gh-pages from master bfd48beaf01e729d7d5058ce6e0443a503bcca79

---
+++ precache-manifest.ac6e7384e48ea10d59a78b0e89f2a043.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da2ec9c7f2f0e539fb9ed5bc5290a28d",
+    "url": "/cra-pwa/index.html"
+  },
+  {
+    "revision": "437ce1e617208e347229",
+    "url": "/cra-pwa/static/css/2.c7b90ac4.chunk.css"
+  },
+  {
+    "revision": "a6adb44cf6eb130de170",
+    "url": "/cra-pwa/static/css/4.ff5cc662.chunk.css"
+  },
+  {
+    "revision": "680b3d3494097f0a27a1",
+    "url": "/cra-pwa/static/css/5.c0ca37fc.chunk.css"
+  },
+  {
+    "revision": "9b50eda0d46bdeeea3de",
+    "url": "/cra-pwa/static/css/6.590d5383.chunk.css"
+  },
+  {
+    "revision": "08eebe0d13cf7a9cffd8",
+    "url": "/cra-pwa/static/css/7.9e89eddd.chunk.css"
+  },
+  {
+    "revision": "d731a7ec80c11d68d98d",
+    "url": "/cra-pwa/static/css/8.be0a0328.chunk.css"
+  },
+  {
+    "revision": "af1a3b7d498ed21a2269",
+    "url": "/cra-pwa/static/css/main.5a27b0c4.chunk.css"
+  },
+  {
+    "revision": "68834665e6f4e0ae4db8",
+    "url": "/cra-pwa/static/js/10.2b57199f.chunk.js"
+  },
+  {
+    "revision": "437ce1e617208e347229",
+    "url": "/cra-pwa/static/js/2.5d11d149.chunk.js"
+  },
+  {
+    "revision": "2c6fedbabd6a21fa5f4a64660c63a0c5",
+    "url": "/cra-pwa/static/js/2.5d11d149.chunk.js.LICENSE"
+  },
+  {
+    "revision": "26fdb7d583a9f12c2a8b",
+    "url": "/cra-pwa/static/js/3.a0048bf7.chunk.js"
+  },
+  {
+    "revision": "a6adb44cf6eb130de170",
+    "url": "/cra-pwa/static/js/4.a3af086d.chunk.js"
+  },
+  {
+    "revision": "680b3d3494097f0a27a1",
+    "url": "/cra-pwa/static/js/5.4db54d53.chunk.js"
+  },
+  {
+    "revision": "9b50eda0d46bdeeea3de",
+    "url": "/cra-pwa/static/js/6.19d8f54a.chunk.js"
+  },
+  {
+    "revision": "08eebe0d13cf7a9cffd8",
+    "url": "/cra-pwa/static/js/7.420228bb.chunk.js"
+  },
+  {
+    "revision": "d731a7ec80c11d68d98d",
+    "url": "/cra-pwa/static/js/8.a783a118.chunk.js"
+  },
+  {
+    "revision": "cd6e41f79d5df2a8bdaf",
+    "url": "/cra-pwa/static/js/9.d13a32a0.chunk.js"
+  },
+  {
+    "revision": "af1a3b7d498ed21a2269",
+    "url": "/cra-pwa/static/js/main.a49692b4.chunk.js"
+  },
+  {
+    "revision": "db9f710d7ef356036d4b",
+    "url": "/cra-pwa/static/js/runtime-main.cff13277.js"
+  },
+  {
+    "revision": "947f5bf4f69baf0ea8ecb7510e3c2664",
+    "url": "/cra-pwa/static/media/authBg.947f5bf4.svg"
+  },
+  {
+    "revision": "148f17f36f84c218a379a272e9195552",
+    "url": "/cra-pwa/static/media/logo.148f17f3.png"
+  }
+]);
